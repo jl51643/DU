23a7cbfe5a6398647ad20204f0494895af7106cd
jlukacevic <92021471+jlukacevic@users.noreply.github.com>
2023-05-18 17:20:46 +0200

    Add empty project

---
+++ DU/src/main/java/com/fer/hr/du/DuApplication.java
@@ -0,0 +1,14 @@
+package com.fer.hr.du;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class DuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuApplication.class, args);
+    }
+
+}
---
+++ DU/src/test/java/com/fer/hr/du/DuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fer.hr.du;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
